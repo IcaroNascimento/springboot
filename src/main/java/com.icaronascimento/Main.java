76dd9876a3f674842842c197af62d6ff64d83d33
IcaroNascimento <icaronascimento1@gmail.com>
2018-01-26 01:30:55 -0200

    commit do mvc em springboot

---
+++ src/main/java/com.icaronascimento/Main.java
@@ -0,0 +1,15 @@
+package com.icaronascimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by icaronascimento on 25/01/18.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(  Main.class,args);
+
+    }
+}
